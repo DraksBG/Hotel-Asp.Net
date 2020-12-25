127cf45c51f44b3ac74772a5ad57df5864a7cc79
DraksBG <52380770+DraksBG@users.noreply.github.com>
2020-12-25 13:30:12 +0200

    Create ConferenceHallReservation.cs

---
+++ Data/Hotel.Data.Models/ConferenceHallReservation.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Hotel.Data.Common.Models;
+using Hotel.Data.Models.Enums;
+
+namespace Hotel.Data.Models
+{
+    public class ConferenceHallReservation : BaseDeletableModel<string>
+    {
+        public ConferenceHallReservation()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+
+        public string UserId { get; set; }
+
+        public ApplicationUser User { get; set; }
+
+        [Required]
+        [Phone]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [Range(1, 100)]
+        public int NumberOfGuests { get; set; }
+
+        public decimal TotalPrice { get; set; }
+
+        [Required]
+        public ConfHallEventType EventType { get; set; }
+
+        [Required]
+        public DateTime EventDate { get; set; }
+
+        [Required]
+        [DataType(DataType.Time)]
+        public DateTime CheckIn { get; set; }
+
+        [Required]
+        [DataType(DataType.Time)]
+        public DateTime CheckOut { get; set; }
+
+        public string ConferenceHallId { get; set; }
+
+        public ConferenceHall ConferenceHall { get; set; }
+
+        [MaxLength(300)]
+        public string Message { get; set; }
+    }
+}
