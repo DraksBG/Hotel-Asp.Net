aadf68f0be46fc0eb481f6a0f836752aa6bb8fc2
DraksBG <52380770+DraksBG@users.noreply.github.com>
2020-12-25 14:36:14 +0200

    Update GlobalConstants.cs

--- Hotel.Common/GlobalConstants.cs
+++ Hotel.Common/GlobalConstants.cs
@@ -5,5 +5,8 @@
         public const string SystemName = "Hotel";
 
         public const string AdministratorRoleName = "Administrator";
+
+        public const string RequiredField = "This field is required";
+        public const string NumberOfBedsRange = "The number of beds can be between 1 and 10";
     }
 }
