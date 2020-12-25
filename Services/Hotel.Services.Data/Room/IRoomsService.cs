5e0547627cf5d87ecbec845ad4d1b2c207cd8bba
DraksBG <52380770+DraksBG@users.noreply.github.com>
2020-12-25 15:06:04 +0200

    Create IRoomsService.cs

---
+++ Services/Hotel.Services.Data/Room/IRoomsService.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Hotel.Web.ViewModels.InputModels.Room;
+using Hotel.Web.ViewModels.RoomViewModels;
+
+namespace Hotel.Services.Data.Room
+{
+    public interface IRoomsService
+    {
+        Task<bool> CreateRoomAsync(CreateRoomInputModel input);
+
+        Task<bool> EditRoomAsync(string id, EditRoomViewModel input);
+
+        Task<bool> DeleteRoom(string id);
+
+        Task<IEnumerable<TViewModel>> GetAllRoomsAsync<TViewModel>();
+
+        Task<TViewModel> GetRoomAsync<TViewModel>(string id);
+
+        Task<EditRoomViewModel> GetRoomForEditAsync(string id);
+
+        Task<IEnumerable<TViewModel>> GetAllReservationsAsync<TViewModel>(string userId);
+
+        Task<IEnumerable<TViewModel>> GetAllReservationsAsyncForAdmin<TViewModel>();
+
+        Task<bool> ReserveRoom(ReserveRoomInputModel input);
+
+        Hotel.Data.Models.Room GetRoomById(string id);
+    }
+}
