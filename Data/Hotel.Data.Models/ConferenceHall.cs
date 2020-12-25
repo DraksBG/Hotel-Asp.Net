90361ddb2165c84f3bf9fd97d30dd8af8db4af55
DraksBG <52380770+DraksBG@users.noreply.github.com>
2020-12-25 13:30:34 +0200

    Create ConferenceHall.cs

---
+++ Data/Hotel.Data.Models/ConferenceHall.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Hotel.Data.Common.Models;
+using Hotel.Data.Models.Enums;
+
+namespace Hotel.Data.Models
+{
+    public class ConferenceHall : BaseDeletableModel<string>
+    {
+        public ConferenceHall()
+        {
+            this.Id = Guid.NewGuid().ToString();
+            this.Pictures = new List<Picture>();
+            this.ConferenceHallReservations = new HashSet<ConferenceHallReservation>();
+        }
+
+        [Required]
+        public ConfHallEventType EventType { get; set; }
+
+        public IList<Picture> Pictures { get; set; }
+
+        [Required]
+        [MaxLength(800)]
+        public string Description { get; set; }
+
+        [Required]
+        [Range(0, 100)]
+        public int CurrentCapacity { get; set; }
+
+        [Required]
+        [Range(0, 100)]
+        public int MaxCapacity { get; set; }
+
+        [Required]
+        [Range(0, 100)]
+        public decimal Price { get; set; }
+
+        public ICollection<ConferenceHallReservation> ConferenceHallReservations { get; set; }
+    }
+}
