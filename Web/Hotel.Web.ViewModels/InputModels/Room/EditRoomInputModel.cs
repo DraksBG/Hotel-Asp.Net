e8715438a367975668624913a6a993e770429a5e
DraksBG <52380770+DraksBG@users.noreply.github.com>
2020-12-25 14:49:53 +0200

    Create EditRoomInputModel.cs

---
+++ Web/Hotel.Web.ViewModels/InputModels/Room/EditRoomInputModel.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Hotel.Common;
+using Hotel.Data.Models.Enums;
+
+namespace Hotel.Web.ViewModels.InputModels.Room
+{
+    public class EditRoomInputModel
+    {
+        [Required(ErrorMessage = GlobalConstants.RequiredField)]
+        public RoomType RoomType { get; set; }
+
+        [Required(ErrorMessage = GlobalConstants.RequiredField)]
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage = GlobalConstants.RequiredField)]
+        [Range(1, 10, ErrorMessage = GlobalConstants.NumberOfBedsRange)]
+        public int NumberOfBeds { get; set; }
+
+        public bool HasBathroom { get; set; }
+
+        public bool HasRoomService { get; set; }
+
+        public bool HasSeaView { get; set; }
+
+        public bool HasMountainView { get; set; }
+
+        public bool HasWifi { get; set; }
+
+        public bool HasTv { get; set; }
+
+        public bool HasPhone { get; set; }
+
+        public bool HasAirConditioner { get; set; }
+
+        public bool HasHeater { get; set; }
+    }
+}
