48eed53e75ceea138f2b37a6b88dbb1d14b9edbb
DraksBG <52380770+DraksBG@users.noreply.github.com>
2020-12-25 13:29:17 +0200

    AddedEnumForConf

---
+++ Data/Hotel.Data.Models/Enums/ConfHallEventType.cs
@@ -0,0 +1,10 @@
+﻿namespace Hotel.Data.Models.Enums
+{
+    public enum ConfHallEventType
+    {
+        Conference = 1,
+        TeamBuilding = 2,
+        Sports = 3,
+        BusinessMeeting = 4,
+    }
+}
