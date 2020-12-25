92bbf91ae6161aa19e04b88e9f6e2fe58af98499
DraksBG <52380770+DraksBG@users.noreply.github.com>
2020-12-25 14:44:20 +0200

    Create RestaurantViewModel.cs

---
+++ Web/Hotel.Web.ViewModels/Restaurant/RestaurantViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Hotel.Web.ViewModels.Restaurant
+{
+    public class RestaurantViewModel
+    {
+        public IEnumerable<RestaurantAllViewModel> AllReservations { get; set; }
+
+    }
+}
