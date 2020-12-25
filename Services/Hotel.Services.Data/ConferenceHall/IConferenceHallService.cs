dd5de71f4ed0ff52b490e5954995bc1826b8ca1d
DraksBG <52380770+DraksBG@users.noreply.github.com>
2020-12-25 15:12:49 +0200

    Create IConferenceHallService.cs

---
+++ Services/Hotel.Services.Data/ConferenceHall/IConferenceHallService.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Hotel.Web.ViewModels.InputModels.ConferenceHall;
+
+namespace Hotel.Services.Data.ConferenceHall
+{
+    public interface IConferenceHallService
+    {
+        Task<bool> ReserveConferenceHall(ConferenceHallInputModel input);
+
+        Task<IEnumerable<TViewModel>> GetAllReservationsAsync<TViewModel>(string userId);
+
+        Task<IEnumerable<TViewModel>> GetAllReservationsAsyncForAdmin<TViewModel>();
+
+        Task<int> GetAllHallsAsync<TViewModel>(ConferenceHallInputModel input);
+    }
+}
