8acdb1ecaee0374dac369cf682ed4d972a05ec10
DraksBG <52380770+DraksBG@users.noreply.github.com>
2020-12-25 14:49:40 +0200

    Create ReserveRoomInputModel.cs

---
+++ Web/Hotel.Web.ViewModels/InputModels/Room/ReserveRoomInputModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using AutoMapper;
+using Hotel.Common;
+using Hotel.Services.Mapping;
+using Hotel.Web.Infrastructure;
+
+namespace Hotel.Web.ViewModels.InputModels.Room
+{
+    public class ReserveRoomInputModel : IHaveCustomMappings
+    {
+        [MaxLength(20, ErrorMessage = GlobalConstants.UserNameMaxLength)]
+        public string FirstName { get; set; }
+
+        [MaxLength(20)]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = GlobalConstants.RequiredField)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = GlobalConstants.RequiredField)]
+        [Phone]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = GlobalConstants.RequiredField)]
+        [Range(1, 10, ErrorMessage = GlobalConstants.CountOfPeopleInRoomLength)]
+        public int CountOfPeople { get; set; }
+
+        [Required(ErrorMessage = GlobalConstants.RequiredField)]
+        [DataType(DataType.Date)]
+        [DateCheckAnnotation(ErrorMessage = GlobalConstants.CheckDateTimeAttribute)]
+        public DateTime CheckIn { get; set; }
+
+        [Required(ErrorMessage = GlobalConstants.RequiredField)]
+        [DataType(DataType.Date)]
+        [DateCheckAnnotation(ErrorMessage = GlobalConstants.CheckDateTimeAttribute)]
+        public DateTime CheckOut { get; set; }
+
+        [MaxLength(300, ErrorMessage = GlobalConstants.ContentMessageMaxLength)]
+        public string Message { get; set; }
+
+        public string RoomId { get; set; }
+
+        public string UserId { get; set; }
+
+        public ICollection<string> Pictures { get; set; }
+
+        public void CreateMappings(IProfileExpression configuration)
+        {
+            _ = configuration.CreateMap<Data.Models.Room, ReserveRoomInputModel>()
+            .ForMember(x => x.Pictures, cfg => cfg.MapFrom(x => x.Pictures.Select(pic => pic.Url)));
+        }
+    }
+}
