e22f28b8be0e98e1380f00701ad79d464738b7b0
DraksBG <52380770+DraksBG@users.noreply.github.com>
2020-12-25 14:54:55 +0200

    Create UsersService.cs

---
+++ Services/Hotel.Services.Data/User/UsersService.cs
@@ -0,0 +1,34 @@
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+using Hotel.Data.Common.Repositories;
+using Hotel.Data.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace Hotel.Services.Data.User
+{
+    public class UsersService : IUsersService
+    {
+        private readonly IDeletableEntityRepository<ApplicationUser> context;
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public UsersService(IDeletableEntityRepository<ApplicationUser> context, UserManager<ApplicationUser> userManager)
+        {
+            this.context = context;
+            this.userManager = userManager;
+        }
+
+        public async Task<string> GetUserIdAsync(ClaimsPrincipal claims)
+        {
+            ApplicationUser uID = await this.userManager.GetUserAsync(claims);
+
+            return uID.Id;
+        }
+
+        public async Task<string> GetUserPhoneNumberAsync(ClaimsPrincipal claims)
+        {
+            ApplicationUser uID = await this.userManager.GetUserAsync(claims);
+
+            return uID.PhoneNumber;
+        }
+    }
+}
