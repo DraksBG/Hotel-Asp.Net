15b4eece60433a6699199796fe1436edcbd755a3
DraksBG <52380770+DraksBG@users.noreply.github.com>
2020-12-25 15:02:11 +0200

    AddedCloudinaryAPI
    For PictureUpload

---
+++ Services/Hotel.Services.Data/Cloudinary/CloudinaryService.cs
@@ -0,0 +1,44 @@
+﻿using CloudinaryDotNet.Actions;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using System.Threading.Tasks;
+using CloudinaryDotNet;
+
+namespace Hotel.Services.Data.Cloudinary
+{
+    public class CloudinaryService : ICloudinaryService
+    {
+        private readonly CloudinaryDotNet.Cloudinary cloudinary;
+
+        public CloudinaryService(CloudinaryDotNet.Cloudinary cloudinary)
+        {
+            this.cloudinary = cloudinary;
+        }
+
+        public async Task<string> UploadPhotoAsync(IFormFile picture, string name, string folderName)
+        {
+            byte[] destinationData;
+
+            using (var ms = new MemoryStream())
+            {
+                await picture.CopyToAsync(ms);
+                destinationData = ms.ToArray();
+            }
+
+            UploadResult uploadResult = null;
+
+            using (var ms = new MemoryStream(destinationData))
+            {
+                ImageUploadParams uploadParams = new ImageUploadParams
+                {
+                    Folder = folderName,
+                    File = new FileDescription(name, ms),
+                };
+
+                uploadResult = this.cloudinary.Upload(uploadParams);
+            }
+
+            return uploadResult?.SecureUri.AbsoluteUri;
+        }
+    }
+}
---
+++ Services/Hotel.Services.Data/Cloudinary/ICloudinaryService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Hotel.Services.Data.Cloudinary
+{
+    public interface ICloudinaryService
+    {
+        Task<string> UploadPhotoAsync(IFormFile picture, string name, string folderName);
+    }
+}
