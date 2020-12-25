5ba15570b1d54df7ab3d3d17743f9bbd2d6e6d78
DraksBG <52380770+DraksBG@users.noreply.github.com>
2020-12-25 15:02:16 +0200

    Update GlobalConstants.cs

--- Hotel.Common/GlobalConstants.cs
+++ Hotel.Common/GlobalConstants.cs
@@ -8,5 +8,9 @@
 
         public const string RequiredField = "This field is required";
         public const string NumberOfBedsRange = "The number of beds can be between 1 and 10";
+        public const string UserNameMaxLength = "The field can contain no more than 20 characters";
+        public const string CountOfPeopleInRoomLength = "The number must be between 1 and 10.";
+        public const string CheckDateTimeAttribute = "Please enter date after today";
+        public const string ContentMessageMaxLength = "The field can contain no more than 300 characters";
     }
 }
