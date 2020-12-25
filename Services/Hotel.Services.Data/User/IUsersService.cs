39addd98ed3ce5bad171c386da37cb070e974148
DraksBG <52380770+DraksBG@users.noreply.github.com>
2020-12-25 14:54:53 +0200

    Create IUsersService.cs

---
+++ Services/Hotel.Services.Data/User/IUsersService.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Hotel.Services.Data.User
+{
+    public interface IUsersService
+    {
+        Task<string> GetUserIdAsync(ClaimsPrincipal claims);
+
+        Task<string> GetUserPhoneNumberAsync(ClaimsPrincipal claims);
+    }
+}
