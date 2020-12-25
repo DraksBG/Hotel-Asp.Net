fa105169d73938167d96589f964fa8d639c35342
DraksBG <52380770+DraksBG@users.noreply.github.com>
2020-12-25 14:40:01 +0200

    AddedNewModelAndDeletedMIgrations

---
+++ Data/Hotel.Data.Models/ContactForm.cs
@@ -0,0 +1,36 @@
+﻿using Hotel.Data.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Hotel.Data.Models
+{
+    public class ContactForm : BaseDeletableModel<string>
+    {
+        public ContactForm()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+
+        [Required]
+        [MaxLength(20)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string Title { get; set; }
+
+        [Required]
+        [MaxLength(300)]
+        public string Content { get; set; }
+    }
+}
