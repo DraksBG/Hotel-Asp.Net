70f74a51ba3b56d408bb7122c1cdb15e558910a7
DraksBG <52380770+DraksBG@users.noreply.github.com>
2020-12-25 15:06:00 +0200

    Create PictureService.cs

---
+++ Services/Hotel.Services.Data/Picture/PictureService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using Hotel.Common;
+using Hotel.Data.Common.Repositories;
+
+namespace Hotel.Services.Data.Picture
+{
+    public class PictureService : IPictureService
+    {
+        private readonly IDeletableEntityRepository<Hotel.Data.Models.Picture> pictures;
+
+        public PictureService(IDeletableEntityRepository<Hotel.Data.Models.Picture> pictures)
+        {
+            this.pictures = pictures;
+        }
+
+        public async Task<string> AddPictureAsync(string url)
+        {
+            var picture = new Hotel.Data.Models.Picture() { Url = url };
+
+            await this.pictures.AddAsync(picture);
+            var result = await this.pictures.SaveChangesAsync();
+
+            if (result < 0)
+            {
+                throw new InvalidOperationException(GlobalConstants.InvalidOperationExceptionInPictureService);
+            }
+            else
+            {
+                return picture.Id;
+            }
+        }
+    }
+}
