4019d6c45c5a1de79444203ce8f7fe0c519d8905
DraksBG <52380770+DraksBG@users.noreply.github.com>
2020-12-25 15:14:26 +0200

    Create RestaurantInputModel.cs

---
+++ Web/Hotel.Web.ViewModels/InputModels/Restaurant/RestaurantInputModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Hotel.Common;
+using Hotel.Data.Models.Enums;
+using Hotel.Services.Mapping;
+using Hotel.Web.Infrastructure;
+
+namespace Hotel.Web.ViewModels.InputModels.Restaurant
+{
+    public class RestaurantInputModel : IMapFrom<Hotel.Data.Models.Restaurant>
+    {
+        [MaxLength(20, ErrorMessage = GlobalConstants.UserNameMaxLength)]
+        public string FirstName { get; set; }
+
+        [MaxLength(20, ErrorMessage = GlobalConstants.UserNameMaxLength)]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress(ErrorMessage = GlobalConstants.InvalidEmail)]
+        public string EmailAddress { get; set; }
+
+        [Required(ErrorMessage = GlobalConstants.RequiredField)]
+        [Phone(ErrorMessage = GlobalConstants.InvalidPhoneNumber)]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = GlobalConstants.RequiredField)]
+        [Range(1, 100, ErrorMessage = GlobalConstants.RestaurantReserveGuestsMax)]
+        public int NumberOfGuests { get; set; }
+
+        [Required(ErrorMessage = GlobalConstants.RequiredField)]
+        public RestaurantEventType EventType { get; set; }
+
+        [Required(ErrorMessage = GlobalConstants.RequiredField)]
+        [DataType(DataType.Date)]
+        [DateCheckAnnotation(ErrorMessage = GlobalConstants.CheckDateTimeAttribute)]
+        public DateTime EventDate { get; set; }
+
+        [Required(ErrorMessage = GlobalConstants.RequiredField)]
+        [DataType(DataType.Time)]
+        public DateTime CheckIn { get; set; }
+
+        [Required(ErrorMessage = GlobalConstants.RequiredField)]
+        [DataType(DataType.Time)]
+        public DateTime CheckOut { get; set; }
+
+        [MaxLength(300, ErrorMessage = GlobalConstants.ContentMessageMaxLength)]
+        public string Message { get; set; }
+
+        public string UserId { get; set; }
+    }
+}
