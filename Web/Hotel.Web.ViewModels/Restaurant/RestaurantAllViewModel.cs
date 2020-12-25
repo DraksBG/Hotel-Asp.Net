af16b7e01a7c6561e3af0cb330b33ed8009da377
DraksBG <52380770+DraksBG@users.noreply.github.com>
2020-12-25 14:49:37 +0200

    Update RestaurantAllViewModel.cs

--- Web/Hotel.Web.ViewModels/Restaurant/RestaurantAllViewModel.cs
+++ Web/Hotel.Web.ViewModels/Restaurant/RestaurantAllViewModel.cs
@@ -1,10 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Hotel.Data.Models;
+using Hotel.Services.Mapping;
 
 namespace Hotel.Web.ViewModels.Restaurant
 {
-    class RestaurantAllViewModel
+    public class RestaurantAllViewModel : IMapFrom<RestaurantReservation>
     {
+        public ApplicationUser User { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string EventType { get; set; }
+
+        public int NumberOfGuests { get; set; }
+
+        public DateTime EventDate { get; set; }
+
+        public DateTime CheckIn { get; set; }
+
+        public DateTime CheckOut { get; set; }
+
+        public decimal TotalPrice { get; set; }
     }
 }
