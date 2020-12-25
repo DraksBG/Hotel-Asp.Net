df5930c13048d7dcbb44578c1db4331f09dee424
DraksBG <52380770+DraksBG@users.noreply.github.com>
2020-12-25 14:49:50 +0200

    Create CreateRoomInputModel.cs

---
+++ Web/Hotel.Web.ViewModels/InputModels/Room/CreateRoomInputModel.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Hotel.Common;
+using Hotel.Data.Models.Enums;
+using Microsoft.AspNetCore.Http;
+
+namespace Hotel.Web.ViewModels.InputModels.Room
+{
+    public class CreateRoomInputModel
+    {
+        [Required(ErrorMessage = GlobalConstants.RequiredField)]
+        public RoomType RoomType { get; set; }
+
+        [Required(ErrorMessage = GlobalConstants.RequiredField)]
+        [Range(typeof(decimal), "1", "1000")]
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage = GlobalConstants.RequiredField)]
+        [Range(1, 10, ErrorMessage = GlobalConstants.NumberOfBedsRange)]
+        public int NumberOfBeds { get; set; }
+
+        public bool HasBathroom { get; set; }
+
+        public bool HasRoomService { get; set; }
+
+        public bool HasSeaView { get; set; }
+
+        public bool HasMountainView { get; set; }
+
+        public bool HasWifi { get; set; }
+
+        public bool HasTv { get; set; }
+
+        public bool HasPhone { get; set; }
+
+        public bool HasAirConditioner { get; set; }
+
+        public bool HasHeater { get; set; }
+
+        [Required(ErrorMessage = GlobalConstants.RequiredField)]
+        [DataType(DataType.Upload)]
+        public ICollection<IFormFile> Pictures { get; set; }
+    }
+}
