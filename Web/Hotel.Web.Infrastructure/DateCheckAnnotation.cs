1200e93bb8c3017b0b64c3dfb11be12aae5541b9
DraksBG <52380770+DraksBG@users.noreply.github.com>
2020-12-25 14:50:35 +0200

    CreatedRoomViewModelAndAddedRefferenceTowardsInfrastructure

---
+++ Web/Hotel.Web.Infrastructure/DateCheckAnnotation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Hotel.Web.Infrastructure
+{
+    public class DateCheckAnnotation : ValidationAttribute
+    {
+        public DateCheckAnnotation()
+        {
+        }
+
+        public override bool IsValid(object value)
+        {
+            var dt = (DateTime)value;
+            if (dt.Day >= DateTime.Now.Day)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
