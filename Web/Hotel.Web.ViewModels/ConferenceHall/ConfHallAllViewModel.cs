ed2494cfb0df74df83ebba66ba8c2d6fc6fce2de
DraksBG <52380770+DraksBG@users.noreply.github.com>
2020-12-25 14:45:04 +0200

    Create ConfHallAllViewModel.cs

---
+++ Web/Hotel.Web.ViewModels/ConferenceHall/ConfHallAllViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Hotel.Data.Models;
+using Hotel.Services.Mapping;
+
+namespace Hotel.Web.ViewModels.ConferenceHall
+{
+    public class ConfHallAllViewModel : IMapFrom<ConferenceHallReservation>, IMapFrom<Data.Models.ConferenceHall>
+    {
+        public string ConferenceHallId { get; set; }
+
+        public ApplicationUser User { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string EventType { get; set; }
+
+        public int NumberOfGuests { get; set; }
+
+        public DateTime EventDate { get; set; }
+
+        public DateTime CheckIn { get; set; }
+
+        public DateTime CheckOut { get; set; }
+
+        public decimal TotalPrice { get; set; }
+
+    }
+}
