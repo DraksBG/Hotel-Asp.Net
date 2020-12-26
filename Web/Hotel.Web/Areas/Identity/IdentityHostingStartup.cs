8dea1ee52339d958a758582b02ca0c82857b3f77
DraksBG <52380770+DraksBG@users.noreply.github.com>
2020-12-26 17:11:45 +0200

    Create IdentityHostingStartup.cs

---
+++ Web/Hotel.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Hotel.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => { });
+        }
+    }
+}
