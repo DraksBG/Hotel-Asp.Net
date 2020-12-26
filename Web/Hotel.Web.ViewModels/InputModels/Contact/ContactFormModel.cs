eaad5aa15d30db1b292685f5118871ef43e7bb83
DraksBG <52380770+DraksBG@users.noreply.github.com>
2020-12-26 21:48:58 +0200

    Create ContactFormModel.cs

---
+++ Web/Hotel.Web.ViewModels/InputModels/Contact/ContactFormModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using GuestHouse.Common;
+
+namespace GuestHouse.Web.ViewModels.InputModels.Contact
+{
+    public class ContactFormModel
+    {
+        [Required(ErrorMessage = GlobalConstants.RequiredField)]
+        [MaxLength(20, ErrorMessage = GlobalConstants.UserNameMaxLength)]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = GlobalConstants.RequiredField)]
+        [MaxLength(20, ErrorMessage = GlobalConstants.UserNameMaxLength)]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = GlobalConstants.RequiredField)]
+        [EmailAddress(ErrorMessage = GlobalConstants.InvalidEmail)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = GlobalConstants.RequiredField)]
+        [MaxLength(30, ErrorMessage = GlobalConstants.ContactFormTitleMaxLength)]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = GlobalConstants.RequiredField)]
+        [MaxLength(300, ErrorMessage = GlobalConstants.ContentMessageMaxLength)]
+        public string Content { get; set; }
+    }
+}
