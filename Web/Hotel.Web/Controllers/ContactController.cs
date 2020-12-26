d7766727c6472c4a4141ea2c167ab161e2b0a400
DraksBG <52380770+DraksBG@users.noreply.github.com>
2020-12-26 21:49:01 +0200

    Create ContactController.cs

---
+++ Web/Hotel.Web/Controllers/ContactController.cs
@@ -0,0 +1,51 @@
+﻿using System.Threading.Tasks;
+using GuestHouse.Common;
+using GuestHouse.Web.ViewModels.InputModels.Contact;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace GuestHouse.Web.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly IConfiguration configuration;
+
+        public ContactController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(ContactFormModel model)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.View(model);
+            }
+
+            var apiKey = this.configuration.GetValue<string>("SendGrid:ApiKey");
+
+            var client = new SendGridClient(apiKey);
+            var name = model.FirstName + " " + model.LastName;
+            var from = new EmailAddress(model.Email, name);
+            var subject = model.Title;
+            var to = new EmailAddress("paradiseguesthouse@abv.bg", "ParadiseGuestHouse");
+            var plainTextContent = model.Content;
+            var htmlContent = $"<p>{model.Content}</p>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+
+            await client.SendEmailAsync(msg);
+
+            this.TempData["InfoMessage"] = GlobalConstants.SuccessfullySentAnEmail;
+
+            return this.Redirect("/");
+        }
+    }
+}
