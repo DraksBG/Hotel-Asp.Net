140a9a446a8c28f94845df95081c495618079d17
DraksBG <52380770+DraksBG@users.noreply.github.com>
2020-12-26 21:49:50 +0200

    Update ContactController.cs

--- Web/Hotel.Web/Controllers/ContactController.cs
+++ Web/Hotel.Web/Controllers/ContactController.cs
@@ -1,12 +1,12 @@
 ﻿using System.Threading.Tasks;
-using GuestHouse.Common;
-using GuestHouse.Web.ViewModels.InputModels.Contact;
+using Hotel.Common;
+using Hotel.Web.ViewModels.InputModels.Contact;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using SendGrid;
 using SendGrid.Helpers.Mail;
 
-namespace GuestHouse.Web.Controllers
+namespace Hotel.Web.Controllers
 {
     public class ContactController : Controller
     {
@@ -36,7 +36,7 @@ namespace GuestHouse.Web.Controllers
             var name = model.FirstName + " " + model.LastName;
             var from = new EmailAddress(model.Email, name);
             var subject = model.Title;
-            var to = new EmailAddress("paradiseguesthouse@abv.bg", "ParadiseGuestHouse");
+            var to = new EmailAddress("dimitarkrustanov@abv.bg", "Hotel");
             var plainTextContent = model.Content;
             var htmlContent = $"<p>{model.Content}</p>";
             var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
