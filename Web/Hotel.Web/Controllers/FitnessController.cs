d938ff13431cc1dbb355723a7b91767e389a8e35
DraksBG <52380770+DraksBG@users.noreply.github.com>
2020-12-26 22:03:07 +0200

    Create FitnessController.cs

---
+++ Web/Hotel.Web/Controllers/FitnessController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hotel.Web.Controllers
+{
+    public class FitnessController : Controller
+    {
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
