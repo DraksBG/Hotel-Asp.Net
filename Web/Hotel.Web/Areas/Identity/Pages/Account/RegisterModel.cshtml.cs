23c5a4edbc31c901704a48b106be53efcb039a8b
DraksBG <52380770+DraksBG@users.noreply.github.com>
2020-12-26 16:53:59 +0200

    Create RegisterModel.cshtml.cs

---
+++ Web/Hotel.Web/Areas/Identity/Pages/Account/RegisterModel.cshtml.cs
@@ -0,0 +1,149 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Hotel.Common;
+using Hotel.Data.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Logging;
+
+namespace Hotel.Web.Areas.Identity.Pages.Account
+{
+    [AllowAnonymous]
+    public class RegisterModel : PageModel
+    {
+        private readonly SignInManager<ApplicationUser> signInManager;
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly ILogger<RegisterModel> logger;
+        private readonly IEmailSender emailSender;
+
+        public RegisterModel(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            ILogger<RegisterModel> logger,
+            IEmailSender emailSender)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+            this.logger = logger;
+            this.emailSender = emailSender;
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public string ReturnUrl { get; set; }
+
+        public IList<AuthenticationScheme> ExternalLogins { get; set; }
+
+        public class InputModel
+        {
+            [Required]
+            [StringLength(20, ErrorMessage = "Вашето {0} трябва да е поне {2} и най много {1} символа.", MinimumLength = 4)]
+            [Display(Name = "Име")]
+            public string FirstName { get; set; }
+
+            [Required]
+            [StringLength(20, ErrorMessage = "Вашата {0} трябва да е поне {2} и най много {1} символа.", MinimumLength = 4)]
+            [Display(Name = "Фамилия")]
+            public string LastName { get; set; }
+
+            [Required]
+            [Display(Name = "Телефонен номер")]
+            [RegularExpression(@"08[789]\d{7}", ErrorMessage = "Грешен телефонен номер")]
+            public string PhoneNumber { get; set; }
+
+            [Required]
+            [EmailAddress]
+            [Display(Name = "Имейл")]
+            public string Email { get; set; }
+
+            [Required]
+            [StringLength(100, ErrorMessage = "{0}та трябва да бъде поне {2} и не по-дълга от {1} символа", MinimumLength = 6)]
+            [DataType(DataType.Password)]
+            [Display(Name = "Парола")]
+            public string Password { get; set; }
+
+            [DataType(DataType.Password)]
+            [Display(Name = "Повтори паролата")]
+            [Compare("Password", ErrorMessage = "Паролите не съвпадат.")]
+            public string ConfirmPassword { get; set; }
+
+        }
+
+        public async Task OnGetAsync(string returnUrl = null)
+        {
+            this.ReturnUrl = returnUrl;
+            this.ExternalLogins = (await this.signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+        }
+
+        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+        {
+            returnUrl ??= this.Url.Content("~/");
+            this.ExternalLogins = (await this.signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+            if (this.ModelState.IsValid)
+            {
+                var user = new ApplicationUser
+                {
+                    UserName = this.Input.Email,
+                    FirstName = this.Input.FirstName,
+                    LastName = this.Input.LastName,
+                    Email = this.Input.Email,
+                    PhoneNumber = this.Input.PhoneNumber,
+                };
+
+                var isRoot = !this.userManager.Users.Any();
+
+                var result = await this.userManager.CreateAsync(user, this.Input.Password);
+                if (result.Succeeded)
+                {
+                    if (isRoot)
+                    {
+                        await this.userManager.AddToRoleAsync(user, GlobalConstants.AdministratorRoleName);
+                    }
+
+                    this.logger.LogInformation("Потребителят създаде нов акаунт.");
+
+                    var code = await this.userManager.GenerateEmailConfirmationTokenAsync(user);
+                    code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+                    var callbackUrl = this.Url.Page(
+                        "/Account/ConfirmEmail",
+                        pageHandler: null,
+                        values: new { area = "Identity", userId = user.Id, code = code },
+                        protocol: this.Request.Scheme);
+
+                    await this.emailSender.SendEmailAsync(
+                        this.Input.Email,
+                        "Потвърдете имейла си",
+                        $"Моля, потвърдете акаунта си < a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>като натиснете тук</a>.");
+
+                    if (this.userManager.Options.SignIn.RequireConfirmedAccount)
+                    {
+                        return this.RedirectToPage("RegisterConfirmation", new { email = this.Input.Email });
+                    }
+                    else
+                    {
+                        await this.signInManager.SignInAsync(user, isPersistent: false);
+                        return this.LocalRedirect(returnUrl);
+                    }
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    this.ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+
+            // If we got this far, something failed, redisplay form
+            return this.Page();
+        }
+    }
+}
