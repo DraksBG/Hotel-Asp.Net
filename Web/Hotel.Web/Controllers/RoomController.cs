b8407ee03108e7df0f92e741624f527b2593905d
DraksBG <52380770+DraksBG@users.noreply.github.com>
2020-12-26 16:53:35 +0200

    InstaledBootstrapThemes

---
+++ Web/Hotel.Web/Controllers/RoomController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Hotel.Data.Common.Repositories;
+using Hotel.Data.Models;
+using Hotel.Services.Data.Room;
+using Hotel.Services.Data.User;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hotel.Web.Controllers
+{
+    public class RoomController : Controller
+    {
+        private readonly IRoomsService roomsService;
+        private readonly IUsersService usersService;
+        private readonly IDeletableEntityRepository<Picture> picturesRepository;
+
+        public RoomController(
+            IRoomsService roomsService,
+            IUsersService usersService,
+            IDeletableEntityRepository<Picture> picturesRepository)
+        {
+            this.roomsService = roomsService;
+            this.usersService = usersService;
+            this.picturesRepository = picturesRepository;
+        }
+    }
+}
