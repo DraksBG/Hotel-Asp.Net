b4f2e2dc1d1019dad2ed610645e2be56581213fe
DraksBG <52380770+DraksBG@users.noreply.github.com>
2020-12-27 11:10:54 +0200

    Create 20201227091028_SeedData.cs

---
+++ Data/Hotel.Data/Migrations/20201227091028_SeedData.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Hotel.Data.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
