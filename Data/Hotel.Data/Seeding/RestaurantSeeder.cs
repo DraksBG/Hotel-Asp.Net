e96d2e03ce80714d6867cc11172fa06ff0c43695
DraksBG <52380770+DraksBG@users.noreply.github.com>
2020-12-27 11:10:42 +0200

    Create RestaurantSeeder.cs

---
+++ Data/Hotel.Data/Seeding/RestaurantSeeder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Hotel.Data.Models;
+
+namespace Hotel.Data.Seeding
+{
+    public class RestaurantSeeder : ISeeder
+    {
+        public async Task SeedAsync(ApplicationDbContext dbContext, IServiceProvider serviceProvider)
+        {
+            if (dbContext.Restaurants.Any())
+            {
+                return;
+            }
+
+            await dbContext.Restaurants.AddAsync(new Restaurant
+            {
+                Id = "restaurantid",
+                CreatedOn = DateTime.UtcNow,
+                CurrentCapacity = 100,
+                MaxCapacity = 100,
+                IsDeleted = false,
+                Description = "Restaurant",
+                Price = 50,
+                Title = "Restaurant",
+            });
+        }
+    }
+}
